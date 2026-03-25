34af19bce4d1388d000a713afddf7dc5a5947efb
Aarti Bharat Kale <aartikale427@gmail.com>
2026-03-25 17:57:26 +0530

    terraform added

---
+++ Terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami   = "ami-05d2d839d4f73aafb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2"
+  }
+}
