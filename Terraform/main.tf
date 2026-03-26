f2628f5ba8d9ba9a91a31adde560f2f5114ae10f
Aarti Bharat Kale <aartikale427@gmail.com>
2026-03-26 20:00:34 +0530

    backend

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "ec2" {
   ami   = "ami-05d2d839d4f73aafb"
   instance_type = "t2.micro"
   key_name = "key-pair.pem"
-  vpc_security_group_ids = ["]
+  vpc_security_group_ids = ["sg-0de80a35bf4a38319"]
   availability_zone = "ap-south-1"
   tags = {
     Name = "MyEC2"
