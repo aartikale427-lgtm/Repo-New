679c94c466b96f6d7169914f9dc795434317078d
Aarti Bharat Kale <aartikale427@gmail.com>
2026-03-26 19:58:18 +0530

    backend

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,8 +12,11 @@ terraform {
 resource "aws_instance" "ec2" {
   ami   = "ami-05d2d839d4f73aafb"
   instance_type = "t2.micro"
-
+  key_name = "key-pair.pem"
+  vpc_security_group_ids = ["]
+  availability_zone = "ap-south-1"
   tags = {
     Name = "MyEC2"
+    name = "aarti"
   }
 }
