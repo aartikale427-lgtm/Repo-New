cb564a32a79efe4816053e04db5f4eb798d3be5e
Aarti Bharat Kale <aartikale427@gmail.com>
2026-03-26 19:32:00 +0530

    added

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "terraform-100"
+    region = "ap-south-1"
+  }
+}
 
 resource "aws_instance" "ec2" {
   ami   = "ami-05d2d839d4f73aafb"
