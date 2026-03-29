828e29b4257c44752a34df7c9db0568c67f3b460
Aarti Bharat Kale <aartikale427@gmail.com>
2026-03-29 23:50:05 +0530

    added vpc

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "my-vpc" {
     env = "var.env"
   }
 }
-resource "aws_subnet" "pvt-subnet" {
+resource "aws_subnet" "pvt_subnet" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = var.pvt_cidr
   availability_zone = var.pvt_az
@@ -19,7 +19,7 @@ resource "aws_subnet" "pvt-subnet" {
     Name = "${var.project}-pvt_sub"
   }
 }
-resource "aws_subnet" "pub-subnet" {
+resource "aws_subnet" "pub_subnet" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = var.pub_cidr
   availability_zone = var.pub_az
